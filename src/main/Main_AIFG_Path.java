b3c1463dc147d6471590f906fef2e1b6f62cc0c4
holls91 <simoncinaholly@gmail.com>
2016-05-11 19:54:39 +0200

    Update Main_Path for simulating patrol

--- src/main/Main_AIFG_Path.java
+++ src/main/Main_AIFG_Path.java
@@ -14,24 +14,23 @@ import movement.dynamics.SteeringOutput;
 public class Main_AIFG_Path {
 
 	public static void main(String[] args) {
-		Kinematic character = new Kinematic(new Vector(0.0, 0.0), 1.0, new Vector(0.2,0.2), 0.5);
+		Kinematic character = new Kinematic(new Vector(0.0, 0.0), 3.0, new Vector(0.2,0.2), 1.0);
 		Kinematic target = new Kinematic(new Vector(0.0, 0.0), 0.0, new Vector(0.0,0.0), 0.0);
 		
-		Seek seek = new Seek(character, target, 0.1);
+		Seek seek = new Seek(character, target, 0.15);
 		
 		Path path = new Path();
 		path.addNode(new Vector(0.0, 0.0));
 		path.addNode(new Vector(1.0, 2.0));
 		path.addNode(new Vector(10.0, 10.0));
 		path.addNode(new Vector(17.0, 21.0));
-		path.addNode(new Vector(10.0, 10.0));
 		
 		FollowPathFirst fpf = new FollowPathFirst(seek, path, 1, 1);
 		
-		for(int i=0; i<30; i++){
+		for(int i=0; i<100; i++){
 			Optional<SteeringOutput> steering = fpf.getSteering();
 			if(steering.isPresent()){
-				character.update(steering.get(), 1);
+				character.update(steering.get(), 0.7, 1.3);
 				System.out.println("Character: "+character.getPosition()+" - Velocity: "+character.getVelocity());
 				System.out.println("Target: "+fpf.getPath().getPosition(fpf.getCurrentParam()));	
 			}
